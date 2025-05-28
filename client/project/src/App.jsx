75329dc854fce92d1da42aa38bdae0bc06423ea1
RAMA <161234797+justacoder-3@users.noreply.github.com>
2025-05-28 23:35:58 +0530

    Update App.jsx

--- client/project/src/App.jsx
+++ client/project/src/App.jsx
@@ -2,6 +2,8 @@ import HomePage from './components/HomePage.jsx';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from '../pages/authentication/LoginPage.jsx';
 import SignupPage from '../pages/authentication/SignupPage.jsx';
+import CreatePost from './components/posts/CreatePost.jsx';
+import PostPage from './components/posts/PostPage.jsx';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
+          <Route path="/create-post" element={<CreatePost/>}/>
+          <Route path="/postpage" element={<PostPage/>}/>
         </Routes>
       </Router>
     </>
